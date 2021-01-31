f443c07645de6659ebc747327ae7c2388cd6a364
Italo Cunha <cunha@dcc.ufmg.br>
2021-01-31 20:00:41 -0300

    updates

--- bin/delay-monitor-off.sh
+++ bin/delay-monitor-off.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 set -eu
 
-# sleep 2s
-# xset dpms force off
+sleep 2s
+xset dpms force off
 
-if [ ! -f $HOME/.cache/lcd-status ] ; then
-    echo 1 > $HOME/.cache/lcd-status
-fi
+# if [ ! -f $HOME/.cache/lcd-status ] ; then
+#     echo 1 > $HOME/.cache/lcd-status
+# fi
 
-read lcd < $HOME/.cache/lcd-status
-if [ "$lcd" -eq "0" ]; then
-    swaymsg "output * dpms on"
-    echo 1 > $HOME/.cache/lcd-status
-else
-    swaymsg "output * dpms off"
-    echo 0 > $HOME/.cache/lcd-status
-fi
+# read lcd < $HOME/.cache/lcd-status
+# if [ "$lcd" -eq "0" ]; then
+#     swaymsg "output * dpms on"
+#     echo 1 > $HOME/.cache/lcd-status
+# else
+#     swaymsg "output * dpms off"
+#     echo 0 > $HOME/.cache/lcd-status
+# fi
