6ecfd9c7f7c53eb8023785ea0d8b3c3677c0b4d5
Italo Cunha <cunha@dcc.ufmg.br>
2022-09-02 18:15:39 -0300

    add zsh-autoswitch-virtualenv

---
+++ bin/setup-zsh.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+AUTO_VENV_REPO="https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv.git"
+CLONE_PATH=$HOME/bin/zsh-autoswitch-virtualenv
+
+git clone $AUTO_VENV_REPO $CLONE_PATH
