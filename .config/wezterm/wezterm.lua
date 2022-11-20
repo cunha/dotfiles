8c0a98c7e56a8861d800c80aca69b55f3fd212cb
Italo Cunha <cunha@dcc.ufmg.br>
2022-11-20 11:33:40 -0300

    update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,12 +1,9 @@
 -- local wezterm = require 'wezterm'
 local unix_domains = {}
--- unix_domains = require 'unix'
+unix_domains = require 'unix'
 local ssh_domains = {}
--- ssh_domains = require 'ssh'
-
-local keys = {
-    { key = 'c', mods = 'LEADER', action = 'ShowLauncher' },
-}
+ssh_domains = require 'ssh'
+local keys = require 'keys'
 
 return {
   bold_brightens_ansi_colors = false,
@@ -15,12 +12,14 @@ return {
   cursor_blink_ease_in = "Constant",
   cursor_blink_ease_out = "Constant",
   default_cursor_style = "BlinkingBlock",
+  disable_default_key_bindings = true,
   enable_kitty_keyboard = true,
   enable_scroll_bar = false,
   -- font = wezterm.font 'JetBrains Mono' -- JetBrains Mono is the default
   font_size = 13,
   hide_tab_bar_if_only_one_tab = true,
-  keys = keys,
+  key_tables = keys.key_tables,
+  keys = keys.keys,
   -- launch_menu = launch_menu,
   leader = { key = 'KeypadAdd', timeout_milliseconds = 1000 },
   tab_bar_at_bottom = true,
@@ -30,4 +29,7 @@ return {
   ssh_domains = ssh_domains,
   unix_domains = unix_domains,
   use_fancy_tab_bar = false,
+  -- debug_key_events = true,
+  -- KeypadComma is raw:95
+  -- KeypadDecimal is .
 }
