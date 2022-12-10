857bfa386b77e411e8a329fdba6b7d37e6f541ac
Italo Cunha <cunha@dcc.ufmg.br>
2022-12-09 23:35:13 -0300

    update macos configs

--- bin/setup-macos.sh
+++ bin/setup-macos.sh
@@ -4,21 +4,25 @@ set -eu
 xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
  
+brew install atool
 brew install bash
+brew install coreutils
+brew install curl
+brew install gnu-sed
 brew install fzf
 brew install git
 brew install jq
 brew install lazygit
-brew install openvpn
-brew install neovim
+brew install man-db
+brew install mupdf
 brew install showkey
 brew install socat
 brew install tmux
 brew install unzip
+brew install w3m
 brew install wezterm
 brew install wget
 
-brew install bash-language-server
-brew install shellcheck
-brew install lua-language-server
+brew install texlive
+
 
