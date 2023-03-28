f74a49a6897693b666b60dc74852492a12e141a8
Italo Cunha <cunha@dcc.ufmg.br>
2023-03-28 17:18:47 -0300

    update

--- bin/setup-linux-srv.sh
+++ bin/setup-linux-srv.sh
@@ -3,9 +3,9 @@ set -eu
 
 sudo apt install pkg-config libssl-dev
 
-DEBURL=https://github.com/wez/wezterm/releases/download/20221119-145034-49b9839f/wezterm-20221119-145034-49b9839f.Debian11.deb
-DEBFILE=$HOME/.cache/$(basename $DEBURL)
-if [[ ! -e $DEBFILE ]] ; then
-	wget "$DEBURL" -O "$DEBFILE"
-	sudo dpkg -i "$DEBFILE"
-fi
+# DEBURL=https://github.com/wez/wezterm/releases/download/20221119-145034-49b9839f/wezterm-20221119-145034-49b9839f.Debian11.deb
+# DEBFILE=$HOME/.cache/$(basename $DEBURL)
+# if [[ ! -e $DEBFILE ]] ; then
+# 	wget "$DEBURL" -O "$DEBFILE"
+# 	sudo dpkg -i "$DEBFILE"
+# fi
