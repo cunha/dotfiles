04af53241557819f91dbf3a21282658f044c96ff
Italo Cunha <cunha@dcc.ufmg.br>
2023-04-07 01:33:23 -0300

    move python dev requirements

--- bin/setup-dev.sh
+++ bin/setup-dev.sh
@@ -27,15 +27,7 @@ brew install texlab
 brew install virtualenv
 brew install yaml-language-server
 
-pip3 install --user --upgrade 'python-lsp-server[all]'
-pip3 install --user --upgrade black
-pip3 install --user --upgrade flake8
-pip3 install --user --upgrade isort
-pip3 install --user --upgrade pip
-pip3 install --user --upgrade pycodestyle
-pip3 install --user --upgrade pyflakes
-pip3 install --user --upgrade pylint
-pip3 install --user --upgrade rope
+pip3 install --user --upgrade -r $HOME/.config/requirements.txt
 
 AUTO_VENV_REPO="https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv.git"
 CLONE_PATH=$HOME/bin/zsh-autoswitch-virtualenv
