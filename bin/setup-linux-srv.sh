0b256fb0a5eb1e342f5a5a12ab134e324e16f3e7
Italo Cunha <cunha@dcc.ufmg.br>
2023-04-23 12:01:09 -0300

    chore: update wezterm deb URL

--- bin/setup-linux-srv.sh
+++ bin/setup-linux-srv.sh
@@ -3,7 +3,7 @@ set -eu
 
 sudo apt install pkg-config libssl-dev
 
-DEBURL=https://github.com/wez/wezterm/releases/download/20230326-111934-3666303c/wezterm-20230326-111934-3666303c.Debian11.deb
+DEBURL=https://github.com/wez/wezterm/releases/download/20230408-112425-69ae8472/wezterm-20230408-112425-69ae8472.Debian11.deb
 DEBFILE=$HOME/.cache/$(basename $DEBURL)
 if [[ ! -e $DEBFILE ]] ; then
 	wget "$DEBURL" -O "$DEBFILE"
