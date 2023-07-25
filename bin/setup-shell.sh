e99397d55f1a5278bcbb728096d7b0caa6ba5678
Italo Cunha <cunha@dcc.ufmg.br>
2023-07-25 17:04:08 -0300

    chore: remove deprecated installs

--- bin/setup-shell.sh
+++ bin/setup-shell.sh
@@ -11,15 +11,7 @@ rustup update
 rustup upgrade
 cargo install cargo-update
 cargo install markdown2html-converter
-cargo install --git https://github.com/kamiyaa/joshuto.git --force
 cargo install-update -a
-
-if [[ ! -d $HOME/bin/helix ]] ; then
-  git clone https://github.com/helix-editor/helix "$HOME/bin/helix"
-fi
-(cd "$HOME/bin/helix" && git pull && cargo install --locked --path helix-term)
-hx --grammar fetch
-hx --grammar build
 
 # Download and compile wezterm's terminfo database
 curl -o "$HOME/.config/terminfo/wezterm.terminfo" https://raw.githubusercontent.com/wez/wezterm/master/termwiz/data/wezterm.terminfo
@@ -39,6 +31,3 @@ fi
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim \
     --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# git clone --depth 1 https://github.com/wbthomason/packer.nvim \
-#         ~/.local/share/nvim/site/pack/packer/start/packer.nvim
