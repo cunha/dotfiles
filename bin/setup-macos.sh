76f94b52612b3d6e0c010907d08c8aba1414f010
Italo Cunha <cunha@dcc.ufmg.br>
2024-07-22 12:48:37 -0300

    update with dark-mode-notify

--- bin/setup-macos.sh
+++ bin/setup-macos.sh
@@ -4,6 +4,8 @@ set -eu
 # xcode-select --install
 # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+mkdir -p $HOME/.logs
+
 brew install atool \
              ansible \
              bash \
@@ -69,3 +71,18 @@ brew install --cask font-jetbrains-mono \
                     font-noto-color-emoji
 
 ln -s /Applications/Marta.app/Contents/Resources/launcher "$HOME/bin/marta"
+
+echo "Setting up dark-mode-notify"
+mkdir -p $HOME/.logs
+DMNDIR=$HOME/.cache/dark-mode-notify.git
+if [[ ! -d $DMNDIR ]] ; then
+	git clone https://github.com/bouk/dark-mode-notify.git $DMNDIR
+fi
+pushd $HOME/.cache/dark-mode-notify.git &> /dev/null
+git pull
+make
+cp .build/release/dark-mode-notify $HOME/bin/
+popd &> /dev/null
+cp $HOME/.config/dark-mode-notify/ke.bou.dark-mode-notify.plist ~/Library/LaunchAgents/
+launchctl load -w /Users/cunha/Library/LaunchAgents/ke.bou.dark-mode-notify.plist
+
